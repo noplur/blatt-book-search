d448e2dd84b0a86e596b296734f6ed1650943051
noplur <geoduckphoto@gmail.com>
2022-06-18 18:45:58 -0700

    update to cluster

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -129,5 +129,4 @@ const SignupForm = () => {
   );
 };
 
-
 export default SignupForm;
--- server/server.js
+++ server/server.js
@@ -39,3 +39,5 @@ db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
+////
